beea9f990056c60356f97b479afaae632cf06af0
zntb <zenetibi@yahoo.com>
2024-04-15 11:09:14 +0300

    Add Side Menu Component Design

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,21 @@
+import MenuPosts from '../menuPosts/MenuPosts';
+import MenuCategories from '../menuCategories/MenuCategories';
 import styles from './menu.module.css';
 
 const Menu = () => {
-  return <div className={styles.container}>Menu</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>{"What's hot"}</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <MenuPosts withImage={false} />
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories />
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editors Pick</h1>
+      <MenuPosts withImage={true} />
+    </div>
+  );
 };
 
 export default Menu;
