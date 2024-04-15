194a9998d4d3bc77b87aaa6daeee1066d2d1c586
zntb <zenetibi@yahoo.com>
2024-04-15 06:37:36 +0300

    Add Styles, Layouts, Containers and CSS Responsive Layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,30 @@
+import Link from 'next/link';
 import styles from './homepage.module.css';
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <Link href="/">
+      <h1>
+        Lorem, ipsum dolor Lorem ipsum dolor sit, amet consectetur adipisicing
+        elit. Dolores, saepe.
+      </h1>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe maiores
+      suscipit earum, eius et non? Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Vitae eaque asperiores exercitationem rem, eum ullam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit magni fugit
+      impedit delectus saepe possimus, id a commodi dolore, quibusdam
+      praesentium maiores quisquam corrupti sequi earum asperiores eos neque
+      deleniti rem deserunt? Soluta, delectus, minima labore laudantium maxime
+      fugit quas ipsum cumque sit excepturi autem nisi, dicta pariatur itaque
+      quae! Sunt vero asperiores aliquid ullam fugit in voluptatibus, quos
+      assumenda numquam dolores. Rem illum quos possimus, nostrum odio soluta
+      voluptate? Quae maxime eligendi quasi doloribus? Corporis eos illum
+      molestiae neque aut natus, harum voluptatum necessitatibus tempora
+      laudantium error, aspernatur cum expedita. Facere veniam, assumenda sed
+      aut, nam deleniti id maxime exercitationem ut incidunt voluptatibus ad,
+      nesciunt dolor fugiat nobis sapiente fuga doloribus? Qui sed voluptate
+      corporis. Maiores illo incidunt impedit dolorem ratione inventore ab
+      officiis.
+    </Link>
+  );
 }
