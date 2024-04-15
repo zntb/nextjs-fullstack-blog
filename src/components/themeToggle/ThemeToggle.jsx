345d9017689454138f9b897af00b37659f54d174
zntb <zenetibi@yahoo.com>
2024-04-15 08:20:51 +0300

    Add CSS Dark Mode Theme Toggle Design

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,7 +1,14 @@
+import Image from 'next/image';
 import styles from './themeToggle.module.css';
 
 const ThemeToggle = () => {
-  return <div className={styles.container}>ThemeToggle</div>;
+  return (
+    <div className={styles.container}>
+      <Image src="/moon.png" alt="moon" width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" alt="sun" width={14} height={14} />
+    </div>
+  );
 };
 
 export default ThemeToggle;
