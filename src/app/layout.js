271dcae3aa0c890bb6dafb0784ea910fc160763c
zntb <zenetibi@yahoo.com>
2024-04-15 09:12:59 +0300

    Add Context API for theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from 'next/font/google';
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
 import './globals.css';
+import { ThemeContextProvider } from '../context/ThemeContext';
+import ThemeProvider from '../providers/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,13 +16,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <div className="container">
-          <div className="wrapper">
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
-        </div>
+        <ThemeContextProvider>
+          <ThemeProvider>
+            <div className="container">
+              <div className="wrapper">
+                <Navbar />
+                {children}
+                <Footer />
+              </div>
+            </div>
+          </ThemeProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   );
